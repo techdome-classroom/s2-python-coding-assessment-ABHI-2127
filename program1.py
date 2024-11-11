c137ac856e1f72e95d2ec94e31e3b3036426d274
ABHI-2127 <abhishek21.burra@gmail.com>
2024-11-11 16:06:34 +0530

    Added solution for Valid Parentheses problem

--- program1.py
+++ program1.py
@@ -4,7 +4,24 @@ class Solution(object):
         :type s: str
         :rtype: bool
         """
-        pass
+        stack = []
+        # Mapping of closing brackets to their respective opening brackets
+        bracket_map = {')': '(', '}': '{', ']': '['}
+        
+        for char in s:
+            if char in bracket_map:
+                # Pop the topmost element if the stack is not empty, otherwise assign a dummy value
+                top_element = stack.pop() if stack else '#'
+                
+                # If the mapping for the closing bracket doesn't match the stack's top element, return False
+                if bracket_map[char] != top_element:
+                    return False
+            else:
+                # If it's an opening bracket, push onto the stack
+                stack.append(char)
+        
+        # If the stack is empty, all brackets are closed properly
+        return not stack
 
 
 
